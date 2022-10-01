30ba5f2f2fc54a39a9a7435aaee0744f116e1b28
Diego Barbo <diegobarbox@gmail.com>
2022-10-01 12:52:37 -0300

    commiting

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ['safe-eyrie-81261.herokuapp.com']
 
 
 # Application definition
